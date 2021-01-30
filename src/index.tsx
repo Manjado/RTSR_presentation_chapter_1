33c604774d2c8f6ff55893d3ec7f0975d375aea6
malchimowicz <michal.alchimowicz@enovatis.pl>
2021-01-30 16:05:31 +0100

    classes, refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import ReactDom from 'react-dom';
-import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
-import EventComponent from './events/EventComponent';
+//import GuestList from './state/GuestList';
+//import UserSearch from './state/UserSearch';
+//import EventComponent from './events/EventComponent';
+import UserSearch from './refs/UserSearch';
 
 const App = () => {
   return (
     <div>
       {/* <GuestList /> */}
       {/* <UserSearch /> */}
-      <EventComponent />
+      {/* <EventComponent /> */}
+      <UserSearch />
     </div>
   );
 };
